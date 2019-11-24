a4215a9b918c8f430c68b5f6ef2c9de63d6f7b52
JerickWong <42172510+JerickWong@users.noreply.github.com>
2019-11-24 20:16:50 +0800

    inserted answer options, and removed setters

--- app/src/main/java/com/example/whowantstobeamillionaire/Question.java
+++ app/src/main/java/com/example/whowantstobeamillionaire/Question.java
@@ -3,29 +3,47 @@ package com.example.whowantstobeamillionaire;
 public class Question {
     private String question;
     private String answer;
+    private String optionA;
+    private String optionB;
+    private String optionC;
+    private String optionD;
     private int scoreWorth;
+
+    public Question (String ques, String ans, String A, String B, String C, String D, int worth) {
+        question = ques;
+        answer = ans;
+        optionA = A;
+        optionB = B;
+        optionC = C;
+        optionD = D;
+        scoreWorth = worth;
+    }
 
     public String getQuestion() {
         return question;
-    }
-
-    public void setQuestion(String question) {
-        this.question = question;
     }
 
     public String getAnswer() {
         return answer;
     }
 
-    public void setAnswer(String answer) {
-        this.answer = answer;
-    }
-
     public int getScoreWorth() {
         return scoreWorth;
     }
 
-    public void setScoreWorth(int scoreWorth) {
-        this.scoreWorth = scoreWorth;
+    public String getOptionA() {
+        return optionA;
+    }
+
+    public String getOptionB() {
+        return optionB;
+    }
+
+    public String getOptionC() {
+        return optionC;
+    }
+
+    public String getOptionD() {
+        return optionD;
     }
 }
