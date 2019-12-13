44d9044d0367d89863494a2adee05631eb833375
JerickWong <jerick_wong@dlsu.edu.ph>
2019-12-13 21:42:48 +0800

    main game activity get variables

--- app/src/main/java/com/example/whowantstobeamillionaire/MainGameActivity.java
+++ app/src/main/java/com/example/whowantstobeamillionaire/MainGameActivity.java
@@ -3,12 +3,44 @@ package com.example.whowantstobeamillionaire;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainGameActivity extends AppCompatActivity {
 
+    TextView questionTextView;
+    Button aButton, bButton, cButton, dButton;
+    Button score500, score1000, score2000, score3000, score5000, score7500,
+            score15k, score30k, score60k, score125k, score250k, score1M;
+    ImageButton lifeline5050, lifelineDoubleDip, lifelineNextQuestion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_game);
+
+        questionTextView = (TextView) findViewById(R.id.questionTextView);
+        aButton = (Button) findViewById(R.id.aButton);
+        bButton = (Button) findViewById(R.id.bButton);
+        cButton = (Button) findViewById(R.id.cButton);
+        dButton = (Button) findViewById(R.id.dButton);
+
+        score500 = (Button) findViewById(R.id.score500);
+        score1000 = (Button) findViewById(R.id.score1000);
+        score2000 = (Button) findViewById(R.id.score2000);
+        score3000 = (Button) findViewById(R.id.score3000);
+        score5000 = (Button) findViewById(R.id.score5000);
+        score7500 = (Button) findViewById(R.id.score7500);
+        score15k = (Button) findViewById(R.id.score15k);
+        score30k = (Button) findViewById(R.id.score30k);
+        score60k = (Button) findViewById(R.id.score60k);
+        score125k = (Button) findViewById(R.id.score125k);
+        score250k = (Button) findViewById(R.id.score250k);
+        score1M = (Button) findViewById(R.id.score1M);
+
+        lifeline5050 = (ImageButton) findViewById(R.id.lifeline5050);
+        lifelineDoubleDip = (ImageButton) findViewById(R.id.lifelineDoubleDip);
+        lifelineNextQuestion = (ImageButton) findViewById(R.id.lifelineNextQuestion);
     }
 }
+
