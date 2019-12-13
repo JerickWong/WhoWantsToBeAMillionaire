37989383012cb3f9d865f79b1f8e9ae11a38d9a8
JerickWong <jerick_wong@dlsu.edu.ph>
2019-12-13 19:30:48 +0800

    register activity

--- app/src/main/java/com/example/whowantstobeamillionaire/RegisterActivity.java
+++ app/src/main/java/com/example/whowantstobeamillionaire/RegisterActivity.java
@@ -2,18 +2,40 @@ package com.example.whowantstobeamillionaire;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText usernameEditText, passwordEditText, confirmPasswordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+        confirmPasswordEditText = (EditText) findViewById(R.id.confirmPasswordEditText);
     }
 
     public void register(View view) {
-
+        if (usernameEditText.getText().toString().matches(""))
+            Toast.makeText(this, "Enter a username!", Toast.LENGTH_SHORT).show();
+        else if (passwordEditText.getText().toString().matches(""))
+            Toast.makeText(this, "Enter a password!", Toast.LENGTH_SHORT).show();
+        else if (confirmPasswordEditText.getText().toString().matches(""))
+            Toast.makeText(this, "Confirm your password!", Toast.LENGTH_SHORT).show();
+        else if (!passwordEditText.getText().toString().equals(confirmPasswordEditText.getText().toString()))
+            Toast.makeText(this, "Password did not match!", Toast.LENGTH_SHORT).show();
+        else {
+            // MAY CHECKING PA IF USERNAME EXISTS FROM FIREBASE
+            Intent intent = new Intent(this, LoginScreen.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
