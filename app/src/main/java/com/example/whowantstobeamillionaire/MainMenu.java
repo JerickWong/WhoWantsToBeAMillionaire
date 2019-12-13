53b11873c104771d8090e56ca47c3a16055c9230
JerickWong <jerick_wong@dlsu.edu.ph>
2019-12-13 19:47:23 +0800

    main menu

--- app/src/main/java/com/example/whowantstobeamillionaire/MainMenu.java
+++ app/src/main/java/com/example/whowantstobeamillionaire/MainMenu.java
@@ -2,7 +2,9 @@ package com.example.whowantstobeamillionaire;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void startGame(View view) {
+        Intent intent = new Intent(this, MainGameActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void showHighScores(View view) {
+
+        // FIREBASE GET PLAYERS' SCORES
+        // TODO
+    }
+
+    public void quit (View view) {
+        Intent intent = new Intent(this, LoginScreen.class);
+        startActivity(intent);
+        finish();
     }
 }
