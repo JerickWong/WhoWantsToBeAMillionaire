1d8763fd756789d4f59cc7f5887753e53766a530
JerickWong <jerick_wong@dlsu.edu.ph>
2019-12-13 19:22:21 +0800

    forgot to use github, login screen, maingame activity, mainmenu, register

--- app/src/main/java/com/example/whowantstobeamillionaire/MainActivity.java
+++ app/src/main/java/com/example/whowantstobeamillionaire/MainActivity.java
@@ -5,28 +5,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageButton logo;
+    Button registerBtn;
+    EditText usernameTextView, passwordTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        logo = (ImageButton) findViewById(R.id.logo);
-        logo.setBackground(null);
+        setContentView(R.layout.activity_login_screen);
+
+        usernameTextView = (EditText) findViewById(R.id.usernameTextView);
+        passwordTextView = (EditText) findViewById(R.id.passwordTextView);
+
     }
 
-    public void startGame(View v){
-        Intent intent = new Intent(this, GameActivity.class);
-        startActivity(intent);
-        finish();
+    public void login(View v){
+        if (usernameTextView.getText().toString().matches("") || 
+        passwordTextView.getText().toString().matches("")) {
+            Toast.makeText(this, "Enter a username/password", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Intent intent = new Intent(this, GameActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        // FIREBASE LOGIN CHECKER PA
     }
 
-    public void help(View view) {
-        Intent intent = new Intent(this, HelpActivity.class);
+    public void register(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
         finish();
     }
--- app/src/main/java/com/example/whowantstobeamillionaire/RegisterActivity.java
+++ app/src/main/java/com/example/whowantstobeamillionaire/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.example.whowantstobeamillionaire;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void register(View view) {
+
     }
 }
